0eb1688eac93c53a2a202a35b679d36d86c3102e
Abdel Wadoud <abdelwadoudrasmi@gmail.com>
2022-08-10 18:33:15 +0200

    Adding states

---
+++ app/src/main/java/com/beastwall/world/adapter/StatesAdapter.java
@@ -0,0 +1,58 @@
+package com.beastwall.world.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.beastwall.localisation.model.State;
+import com.beastwall.world.R;
+
+import java.util.List;
+
+/**
+ * @author AbdelWadoud Rasmi
+ * <p>
+ * States list adapter
+ */
+public class StatesAdapter extends RecyclerView.Adapter<StatesAdapter.StateHolder> {
+    private List<State> states;
+
+    public StatesAdapter(List<State> states) {
+        this.states = states;
+    }
+
+    @NonNull
+    @Override
+    public StateHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new StateHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.state, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StateHolder holder, int position) {
+        State state = states.get(position);
+        //
+        holder.name.setText(state.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return states == null ? 0 : states.size();
+    }
+
+    /**
+     * StateHolder
+     */
+    public class StateHolder extends RecyclerView.ViewHolder {
+        public TextView name;
+
+        public StateHolder(@NonNull View itemView) {
+            super(itemView);
+            //
+            name = itemView.findViewById(R.id.name);
+        }
+    }
+}
---
+++ app/src/main/java/com/beastwall/world/fragment/StatesFragment.java
@@ -0,0 +1,54 @@
+package com.beastwall.world.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.beastwall.localisation.model.Country;
+import com.beastwall.world.R;
+import com.beastwall.world.adapter.StatesAdapter;
+
+/**
+ * @author AbdelWadoud Rasmi
+ */
+public class StatesFragment extends Fragment {
+    private static StatesFragment instance;
+    private Country country;
+    public RecyclerView recyclerView;
+
+    public StatesFragment(Country country) {
+        this.country = country;
+    }
+
+    /**
+     * get Instance
+     */
+    public static StatesFragment getInstance(Country country) {
+        if (instance == null) {
+            instance = new StatesFragment(country);
+        }
+        return instance;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_states, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        recyclerView = view.findViewById(R.id.states_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new StatesAdapter(country.getStates()));
+    }
+}
